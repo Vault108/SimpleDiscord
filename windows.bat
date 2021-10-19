d9ce6c31e1f5e8f557191b20efe55e8a5ed962ea
Vault108 <44564111+Vault108@users.noreply.github.com>
2021-10-19 08:37:47 -0400

    Fixed windows.bat

--- windows.bat
+++ windows.bat
@@ -1,6 +1,6 @@
 pyinstaller simple_discord.py -w --onefile
 rd /S /Q "__pycache__/" 
 rd /S /Q "build\"
-rd /S /Q "simple_discord.spec"
-move .\dist\simple_discord.exe .\
+del ".\simple_discord.spec"
+move ".\dist\simple_discord.exe .\"
 rd /S /Q "dist/"
