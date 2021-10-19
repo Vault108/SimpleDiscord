dbe07cbb42947017144f0678610cdc8587029935
Vault108 <44564111+Vault108@users.noreply.github.com>
2021-10-19 08:25:14 -0400

    minor changes, version 0.0.10a
    Signed-off-by: Vault108 <44564111+Vault108@users.noreply.github.com>

--- Changelog.md
+++ Changelog.md
@@ -1,10 +1,12 @@
 # Simple Discord
 
 ## 10.19.21
- * Renamed Default-settings.json to settings.json
- * Added a build script to generate an exe / bin file
+ * Renamed Default-settings.json to settings.json.
+ * Added a build script to generate an exe / bin file.
+ * Added a button to remove the logs files. 
+ * File naming tweaks.
 ## 10.18.21
- * Fixed window names
+ * Fixed window names.
 ## 10.10.21
   * Renamed project.
   * Added changelog.
--- simple_discord.py
+++ simple_discord.py
@@ -4,13 +4,14 @@ Written by @Vault108
 """
 import tkinter as tk
 import sys
+import os
 import json
 from tkinter import Tk
 from tkinter import messagebox
 from discord_webhook import DiscordWebhook
 from loguru import logger
 from ratelimit import limits
-__version__ = "0.0.8a"
+__version__ = "0.0.10a"
 
 
 def simple_discord():
@@ -22,15 +23,67 @@ def simple_discord():
     main_window.configure(bg="#738ADB")
     main_window.title("Simple Discord " + __version__)
     main_window.resizable(False, False)
-    tk.Button(main_window, text="Send Message", command=sendawebhok,
-              bd="0", fg="#ffffff", bg="#738ADB",).grid(row=1, column=1)
-    tk.Button(main_window, text="Settings", command=settings, width="20",
-              bd="0", fg="#ffffff", bg="#738ADB").grid(row=2, column=1)
-    tk.Button(main_window, text="Exit", command=bye, width="20",
-              bd="0", fg="#ffffff", bg="#738ADB",).grid(row=3, column=1)
-    tk.Label(main_window, text=__version__, fg="#ffffff",
-             bg="#738ADB").grid(row=3, column=0)
+    tk.Button(
+        main_window,
+        text="Send Message",
+        command=sendawebhok,
+        bd="0",
+        fg="#ffffff",
+        bg="#738ADB",).grid(
+        row=1,
+        column=1)
+    tk.Button(
+        main_window,
+        text="Settings",
+        command=settings,
+        width="20",
+        bd="0",
+        fg="#ffffff",
+        bg="#738ADB").grid(
+        row=2,
+        column=1)
+    tk.Button(
+        main_window,
+        text="Exit",
+        command=bye,
+        width="20",
+        bd="0",
+        fg="#ffffff",
+        bg="#738ADB").grid(
+        row=4,
+        column=1)
+    tk.Label(
+        main_window,
+        text=__version__,
+        fg="#ffffff",
+        bg="#738ADB").grid(
+        row=1,
+        column=0)
+    tk.Button(
+        main_window,
+        text="Delete Logs",
+        command=deletelogs,
+        bd="0",
+        bg="#738ADB",
+        width="10",
+        fg="white").grid(
+        row=3,
+        column=1)
+
     main_window.mainloop()
+
+
+def deletelogs():
+    """
+    Clears Logs
+    """
+    logfolder = "Logs/"
+    try:
+        for files in os.listdir(logfolder):
+            os.remove(os.path.join(logfolder, files))
+            logger.info("Deleted " + files)
+    except PermissionError:
+        pass
 
 
 def settings():
@@ -47,7 +100,10 @@ def settings():
         logger.info("Added Username " + username)
         webhook = urlinputwindow.get("1.0", "end").strip("\n")
         logger.info("Added Webhook ")  # Do not log url for webhook
-        data = {"webhook": webhook, "username": username}
+        data = {
+            "webhook": webhook,
+            "username": username
+        }
         with open("settings.json", "w", encoding="utf-8") as json_file:
             json.dump(data, json_file, sort_keys=True, indent=4)
             json_file.close()
@@ -56,10 +112,22 @@ def settings():
     settings_window.configure(bg="#738ADB")
     settings_window.title("Settings")
     settings_window.resizable(False, False)
-    urlinputwindow = tk.Text(settings_window, height=4)
-    urlinputwindow.grid(row=2, column=1, padx=5, pady=5)
-    usernameinputwindow = tk.Text(settings_window, height=1)
-    usernameinputwindow.grid(row=0, column=1, padx=5, pady=5)
+    urlinputwindow = tk.Text(
+        settings_window,
+        height=4)
+    urlinputwindow.grid(
+        row=2,
+        column=1,
+        padx=5,
+        pady=5)
+    usernameinputwindow = tk.Text(
+        settings_window,
+        height=1)
+    usernameinputwindow.grid(
+        row=0,
+        column=1,
+        padx=5,
+        pady=5)
     tk.Label(
         settings_window,
         fg="white",
@@ -84,7 +152,6 @@ def settings():
         fg="white").grid(
         row=4,
         column=1)
-
     settings_window.mainloop()
 
 
@@ -100,8 +167,8 @@ def sendawebhok():
     """
     Send the web hook
     """
-    @logger.catch
-    @limits(calls=1, period=10)
+    @ logger.catch
+    @ limits(calls=1, period=10)
     def realsend():
         try:
             content = msg.get("1.0", "end")
@@ -111,7 +178,9 @@ def sendawebhok():
                 uname = settingjson["username"]
                 settings_file.close()
             webhook = DiscordWebhook(
-                url=url, username=uname, content=content)
+                url=url,
+                username=uname,
+                content=content)
             webhook.timeout = 20
             response = webhook.execute()
             logger.success(response)
@@ -123,14 +192,35 @@ def sendawebhok():
                 message="Please check your settings.")
     msgbody = tk.Tk()
     msgbody.title("Webhook Messssage Input")
-    msgbody.configure(bg="#738ADB")
+    msgbody.configure(
+        bg="#738ADB")
     msgbody.geometry("")
-    msg = tk.Text(msgbody, height=10)
-    msg.grid(row=2, column=2, padx=5, pady=5)
-    tk.Button(msgbody, text="Send", command=realsend,
-              bd="0", bg="#738ADB", fg="white").grid(row=3, column=2)
-    tk.Button(msgbody, text="Main Menu", command=msgbody.destroy,
-              bd="0", bg="#738ADB", fg="white").grid(row=4, column=2)
+    msg = tk.Text(
+        msgbody,
+        height=10)
+    msg.grid(
+        row=2,
+        column=2,
+        padx=5,
+        pady=5)
+    tk.Button(
+        msgbody,
+        text="Send",
+        command=realsend,
+        bd="0",
+        bg="#738ADB",
+        fg="white").grid(
+        row=3,
+        column=2)
+    tk.Button(
+        msgbody,
+        text="Main Menu",
+        command=msgbody.destroy,
+        bd="0",
+        bg="#738ADB",
+        fg="white").grid(
+        row=4,
+        column=2)
     msgbody.mainloop()
 
 
